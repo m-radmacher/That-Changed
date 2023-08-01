34f742644d8b7fdb92f4bd6094fe194c3dd15dee
Marc Radmacher <marc.radmacher@hetkamp-gmbh.de>
2023-08-01 08:48:50 +0200

    fixed linter issues & dependency update

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
-import { getOctokit } from '@actions/github';
-import * as core from '@actions/core';
-import * as artifact from '@actions/artifact';
-import chalk from 'chalk';
-import PDFDocument from 'pdfkit';
-import * as fs from 'fs';
+import { getOctokit } from "@actions/github";
+import * as core from "@actions/core";
+import * as artifact from "@actions/artifact";
+import chalk from "chalk";
+import PDFDocument from "pdfkit";
+import * as fs from "fs";
 import {
   getEnglishAuthor,
   getEnglishHeader,
@@ -15,9 +15,9 @@ import {
   getGermanOpenInGH,
   getGermanOpenTicket,
   getGermanSubheader,
-} from './i18n';
-import { createMailTransport, sendChangelogMail, verifyMailTransporter } from './mailing';
-import path from 'path';
+} from "./i18n";
+import { createMailTransport, sendChangelogMail, verifyMailTransporter } from "./mailing";
+import path from "path";
 
 export type Commit = {
   author: string | null;
@@ -27,65 +27,65 @@ export type Commit = {
 };
 
 async function run() {
-  const sendEmail = core.getBooleanInput('send_email');
-  const smtpHost = core.getInput('smtp_host');
-  const smtpPort = Number(core.getInput('smtp_port'));
-  const smtpSecure = core.getBooleanInput('smtp_secure');
-  const smtpUser = core.getInput('smtp_username');
-  const smtpPassword = core.getInput('smtp_password');
-  const smtpFrom = core.getInput('smtp_from');
-  const emailTo = core.getInput('email_to').split(';');
+  const sendEmail = core.getBooleanInput("send_email");
+  const smtpHost = core.getInput("smtp_host");
+  const smtpPort = Number(core.getInput("smtp_port"));
+  const smtpSecure = core.getBooleanInput("smtp_secure");
+  const smtpUser = core.getInput("smtp_username");
+  const smtpPassword = core.getInput("smtp_password");
+  const smtpFrom = core.getInput("smtp_from");
+  const emailTo = core.getInput("email_to").split(";");
 
-  const issueTrackerUrlTemplate = core.getInput('issue_tracker_url');
-  const uploadArtifact = core.getBooleanInput('upload');
-  const language = core.getInput('language');
-  const token = core.getInput('token') || process.env.GITHUB_TOKEN || '';
-  const repository = core.getInput('repo').split('/');
+  const issueTrackerUrlTemplate = core.getInput("issue_tracker_url");
+  const uploadArtifact = core.getBooleanInput("upload");
+  const language = core.getInput("language");
+  const token = core.getInput("token") || process.env.GITHUB_TOKEN || "";
+  const repository = core.getInput("repo").split("/");
   if (repository.length !== 2) {
-    core.setFailed('Invalid repository name provided.' + repository);
+    core.setFailed("Invalid repository name provided." + repository);
     return;
   }
   const owner = repository[0];
   const repo = repository[1];
 
-  if (language !== 'de' && language !== 'en') {
-    console.log(chalk.bgRedBright('Invalid language provided. Defaulting to English.'));
+  if (language !== "de" && language !== "en") {
+    console.log(chalk.bgRedBright("Invalid language provided. Defaulting to English."));
   }
 
   // validate email settings
   if (sendEmail === true) {
     if (!smtpHost || !smtpPort || !smtpUser || !smtpPassword || !smtpFrom || !emailTo) {
       core.debug(JSON.stringify({ smtpHost, smtpPort, smtpUser, smtpPassword, smtpFrom, emailTo }));
-      console.log(chalk.bgRedBright('Invalid email settings provided.'));
-      core.setFailed('Invalid email settings provided.');
+      console.log(chalk.bgRedBright("Invalid email settings provided."));
+      core.setFailed("Invalid email settings provided.");
       return;
     }
   }
 
-  console.log('Fetching commits...');
+  console.log("Fetching commits...");
   const { commits, head, base } = await fetchDataFromGitHub(token, owner, repo);
-  console.log('Creating PDF...');
-  core.debug('Temporary folder location: ' + process.env.RUNNER_TEMP);
+  console.log("Creating PDF...");
+  core.debug("Temporary folder location: " + process.env.RUNNER_TEMP);
   createPDF(commits, owner, repo, language, base.name, head.name, issueTrackerUrlTemplate);
-  console.log('Wrote PDF file.');
+  console.log("Wrote PDF file.");
   if (uploadArtifact) {
-    console.log('Uploading artifact...');
-    core.debug('Temporary folder content: ' + fs.readdirSync(process.env.RUNNER_TEMP as string).join('; '));
+    console.log("Uploading artifact...");
+    core.debug("Temporary folder content: " + fs.readdirSync(process.env.RUNNER_TEMP as string).join("; "));
     await artifact
       .create()
       .uploadArtifact(
-        'changelog',
-        [path.join(process.env.RUNNER_TEMP as string, 'output.pdf')],
-        process.env.RUNNER_TEMP as string
+        "changelog",
+        [path.join(process.env.RUNNER_TEMP as string, "output.pdf")],
+        process.env.RUNNER_TEMP as string,
       );
-    console.log('Uploaded artifact.');
+    console.log("Uploaded artifact.");
   }
   if (sendEmail === true) {
-    console.log('Sending email...');
+    console.log("Sending email...");
     const transporter = createMailTransport(smtpHost, smtpPort, smtpSecure, smtpUser, smtpPassword);
     await verifyMailTransporter(transporter);
     await sendChangelogMail(transporter, emailTo, smtpFrom, language, base.name, head.name);
-    console.log('Sent email.');
+    console.log("Sent email.");
   }
 }
 
@@ -96,25 +96,25 @@ function createPDF(
   language: string,
   baseTag: string,
   headTag: string,
-  issueTrackerUrlTemplate: string
+  issueTrackerUrlTemplate: string,
 ) {
   // create empty pdf file
-  fs.closeSync(fs.openSync(path.join(process.env.RUNNER_TEMP as string, 'output.pdf'), 'w'));
+  fs.closeSync(fs.openSync(path.join(process.env.RUNNER_TEMP as string, "output.pdf"), "w"));
   const doc = new PDFDocument();
-  doc.pipe(fs.createWriteStream(path.join(process.env.RUNNER_TEMP as string, 'output.pdf')));
+  doc.pipe(fs.createWriteStream(path.join(process.env.RUNNER_TEMP as string, "output.pdf")));
 
   // list available fonts
-  if (fs.existsSync(path.join(__dirname, 'fonts'))) {
-    core.debug('Available fonts: ' + fs.readdirSync(path.join(__dirname, 'fonts')).join(', '));
+  if (fs.existsSync(path.join(__dirname, "fonts"))) {
+    core.debug("Available fonts: " + fs.readdirSync(path.join(__dirname, "fonts")).join(", "));
   } else {
-    core.debug('fonts folder does not exist');
+    core.debug("fonts folder does not exist");
   }
 
   doc
-    .font(path.join(__dirname, 'fonts', 'Inter.ttf'))
+    .font(path.join(__dirname, "fonts", "Inter.ttf"))
     .fontSize(25)
-    .text(language === 'de' ? getGermanHeader(repo) : getEnglishHeader(repo), {
-      align: 'center',
+    .text(language === "de" ? getGermanHeader(repo) : getEnglishHeader(repo), {
+      align: "center",
       link: `https://github.com/${owner}/${repo}`,
     });
 
@@ -122,8 +122,8 @@ function createPDF(
 
   doc
     .fontSize(17)
-    .text(language === 'de' ? getGermanSubheader(baseTag, headTag) : getEnglishSubheader(baseTag, headTag), {
-      align: 'center',
+    .text(language === "de" ? getGermanSubheader(baseTag, headTag) : getEnglishSubheader(baseTag, headTag), {
+      align: "center",
     });
 
   doc.moveDown(3);
@@ -133,7 +133,7 @@ function createPDF(
       doc
         .fontSize(12)
         .text(commit.message, { continued: true, link: commit.url })
-        .fillColor('#828282')
+        .fillColor("#828282")
         .fontSize(10)
         .text(` (${commit.date?.getDate()}.${commit.date?.getMonth()}.${commit.date?.getFullYear()})`, {
           link: commit.url,
@@ -143,22 +143,22 @@ function createPDF(
     }
     // add author if available
     doc
-      .fillColor('#000000')
+      .fillColor("#000000")
       .fontSize(10)
-      .text(`${language === 'de' ? getGermanAuthor() : getEnglishAuthor()}: ${commit.author}`, { indent: 7 });
+      .text(`${language === "de" ? getGermanAuthor() : getEnglishAuthor()}: ${commit.author}`, { indent: 7 });
     // check for issue tracker references in commit message
-    const issueTrackerRegex = new RegExp('$[a-zA-Z0-9_-]{1,6}$');
+    const issueTrackerRegex = new RegExp("$[a-zA-Z0-9_-]{1,6}$");
     const issueTrackerReferences = issueTrackerRegex.exec(commit.message);
     if (issueTrackerReferences && issueTrackerReferences.length > 0) {
       if (!issueTrackerUrlTemplate) {
-        console.log(chalk.bgYellowBright('Found issue tracker references, but not issue tracker URL was configured.'));
+        console.log(chalk.bgYellowBright("Found issue tracker references, but not issue tracker URL was configured."));
         return;
       }
       for (const match of issueTrackerReferences) {
         doc
-          .fillColor('#000000')
+          .fillColor("#000000")
           .fontSize(10)
-          .text(`${language === 'de' ? getGermanOpenTicket(match) : getEnglishOpenTicket(match)}: ${commit.author}`, {
+          .text(`${language === "de" ? getGermanOpenTicket(match) : getEnglishOpenTicket(match)}: ${commit.author}`, {
             indent: 7,
             link: issueTrackerUrlTemplate.replace(`%ID%`, match.substring(1, match.length - 1)),
           });
@@ -169,7 +169,7 @@ function createPDF(
   }
 
   doc.moveDown(5);
-  doc.text(language === 'de' ? getGermanOpenInGH() : getEnglishOpenInGH(), {
+  doc.text(language === "de" ? getGermanOpenInGH() : getEnglishOpenInGH(), {
     link: `https://github.com/${owner}/${repo}/compare/${baseTag}...${headTag}`,
     underline: true,
   });
@@ -187,7 +187,7 @@ export async function fetchDataFromGitHub(token: string, owner: string, repo: st
   });
 
   if (tags.length < 2) {
-    core.setFailed('Not enough tags found. There need to be at least two tags.');
+    core.setFailed("Not enough tags found. There need to be at least two tags.");
     process.exit(1);
   }
 
--- src/mailing.ts
+++ src/mailing.ts
@@ -1,9 +1,9 @@
-import * as nodemailer from 'nodemailer';
-import SMTPTransport from 'nodemailer/lib/smtp-transport';
-import * as core from '@actions/core';
-import chalk from 'chalk';
-import { getEnglishEmailSubject, getGermanEmailSubject } from './i18n';
-import path from 'path';
+import * as nodemailer from "nodemailer";
+import SMTPTransport from "nodemailer/lib/smtp-transport";
+import * as core from "@actions/core";
+import chalk from "chalk";
+import { getEnglishEmailSubject, getGermanEmailSubject } from "./i18n";
+import path from "path";
 
 export function createMailTransport(host: string, port: number, secure: boolean, user: string, password: string) {
   const transporter = nodemailer.createTransport({
@@ -21,7 +21,7 @@ export function createMailTransport(host: string, port: number, secure: boolean,
 export async function verifyMailTransporter(transporter: nodemailer.Transporter<SMTPTransport.SentMessageInfo>) {
   return transporter.verify((error) => {
     if (error) {
-      console.log(chalk.bgRedBright('Could not establish connection to SMTP server.'));
+      console.log(chalk.bgRedBright("Could not establish connection to SMTP server."));
       core.setFailed(error.message);
     }
   });
@@ -33,16 +33,16 @@ export async function sendChangelogMail(
   from: string,
   language: string,
   head: string,
-  base: string
+  base: string,
 ) {
   for (const recipient of to) {
     await transporter.sendMail({
       from,
       cc: recipient,
-      subject: language === 'de' ? getGermanEmailSubject(base, head) : getEnglishEmailSubject(base, head),
+      subject: language === "de" ? getGermanEmailSubject(base, head) : getEnglishEmailSubject(base, head),
       attachments: [
         {
-          path: path.join(process.env.RUNNER_TEMP as string, 'output.pdf'),
+          path: path.join(process.env.RUNNER_TEMP as string, "output.pdf"),
         },
       ],
     });
